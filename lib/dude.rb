0ce10cdb171cb4aa1ec6092c1b5334dd0027484d
dmikhr <dmitrypkh@gmail.com>
2019-12-20 12:59:03 +0300

    remove unnecessary intance vars

--- lib/dude.rb
+++ lib/dude.rb
@@ -7,14 +7,13 @@ require_relative 'render'
 
 class Dude
   def initialize(params)
-    @params = params
     body = Body.new(params[:name])
 
-    @arms = DrawArms.new(@params, body).arms
-    arms_draw_params = @arms.map { |arm| arm.draw_data }
+    arms = DrawArms.new(params, body).arms
+    arms_draw_params = arms.map { |arm| arm.draw_data }
 
-    @legs = DrawLegs.new(@params, body).legs
-    legs_draw_params = @legs.map { |leg| leg.draw_data }
+    legs = DrawLegs.new(params, body).legs
+    legs_draw_params = legs.map { |leg| leg.draw_data }
 
     @dude_parts = (body.draw_data + arms_draw_params + legs_draw_params).flatten
   end
