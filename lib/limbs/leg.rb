700110e8b34d039a032a9c56c83e71d15ca49f09
dmikhr <dmitrypkh@gmail.com>
2019-12-24 12:43:05 +0300

    leg class refactoring

--- lib/limbs/leg.rb
+++ lib/limbs/leg.rb
@@ -6,32 +6,36 @@ class Leg < Limb
 
   def draw
     super
+    caption_start_x = @x0
+    caption_y_start = @y0
 
     if @params[:conditions].positive?
-      draw_conditions(@params[:conditions], @limb_length, @x0, @y0)
+      draw_conditions
     else
       @draw_data << draw_line(@x0, @y0, @x0, @y0 + @limb_length)
     end
 
-    @draw_data << draw_caption(@params[:name], (@x0 + 10).round, (@y0 + 10).round, 9, :tb)
+    @draw_data << draw_caption(@params[:name], (caption_start_x + 10).round, (caption_y_start + 10).round, 9, :tb)
   end
 
-  def draw_conditions(conditions, limb_length, x0, y0)
-    lines_num = conditions + 1
-    line_length = ((limb_length.abs - conditions * Config::ELLIPSE_LENGTH) / lines_num).round
+  def draw_conditions
+    @lines_num = @params[:conditions] + 1
+    @line_length = ((@limb_length.abs - @params[:conditions] * Config::ELLIPSE_LENGTH) / @lines_num).round
 
-    y1 = y0 + line_length
+    @y1 = @y0 + @line_length
 
-    lines_num.times do |i|
-      @draw_data << draw_line(x0, y0, x0, y1)
+    @lines_num.times { |i| draw_condition(i) }
+  end
 
-      @draw_data << { ellipse: {
-        cx: x0, cy: (y1 + Config::ELLIPSE_LENGTH / 2).round ,
-        rx: (Config::ELLIPSE_LENGTH / 4).round, ry: (Config::ELLIPSE_LENGTH / 2).round
-        }} if i < lines_num - 1
+  def draw_condition(i)
+    @draw_data << draw_line(@x0, @y0, @x0, @y1)
 
-      y0 += line_length + Config::ELLIPSE_LENGTH
-      y1 = y0 + line_length
-    end
+    @draw_data << { ellipse: {
+      cx: @x0, cy: (@y1 + Config::ELLIPSE_LENGTH / 2).round ,
+      rx: (Config::ELLIPSE_LENGTH / 4).round, ry: (Config::ELLIPSE_LENGTH / 2).round
+      }} if i < @lines_num - 1
+
+    @y0 += @line_length + Config::ELLIPSE_LENGTH
+    @y1 = @y0 + @line_length
   end
 end
