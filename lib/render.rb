53f7fab1fbf12a2c896b694509e907f30eae1f7c
dmikhr <dmitrypkh@gmail.com>
2020-01-06 18:41:26 +0300

    add color option to svg render methods

--- lib/render.rb
+++ lib/render.rb
@@ -19,40 +19,40 @@ class Render
     send("svg_#{operator.to_s}".to_sym, params) if defined? operator
   end
 
-  def svg_line(params)
+  def svg_line(params, color = :black)
     <<~LINE.gsub(/\s+/, " ").strip
       <line x1="#{params[:x1]}" y1="#{params[:y1]}"
       x2="#{params[:x2]}"
       y2="#{params[:y2]}"
-      style="stroke:black; stroke-width:1"/>
+      style="stroke:#{color.to_s}; stroke-width:1"/>
     LINE
   end
 
-  def svg_circle(params)
+  def svg_circle(params, color = :black)
     <<~CIRCLE.gsub(/\s+/, " ").strip
       <circle cx="#{params[:cx]}"
       cy="#{params[:cy]}"
       r="#{params[:r]}"
-      fill="white" style="stroke:black; stroke-width:1"/>
+      fill="white" style="stroke:#{color.to_s}; stroke-width:1"/>
     CIRCLE
   end
 
-  def svg_ellipse(params)
+  def svg_ellipse(params, color = :black)
     <<~ELLIPSE.gsub(/\s+/, " ").strip
       <ellipse cx="#{params[:cx]}"
       cy="#{params[:cy]}"
       rx="#{params[:rx]}"
       ry="#{params[:ry]}"
-      style="stroke:black; stroke-width:1" fill="white"/>
+      style="stroke:#{color.to_s}; stroke-width:1" fill="white"/>
     ELLIPSE
   end
 
-  def svg_text(params)
+  def svg_text(params, color = :black)
     <<~TEXT.gsub(/\s+/, " ").strip
       <text x="#{params[:x]}"
       y="#{params[:y]}"
       font-family="arial" font-size="#{params[:font_size]}"
-      fill="black" style="writing-mode:
+      fill="#{color.to_s}" style="writing-mode:
       #{params[:orientation].to_s}">#{params[:caption]}</text>
     TEXT
   end
